d213a287953fad34ef4fdd40c126deaa0fa04e3d
Daniel Nuno <daniel.nunojeda@gmail.com>
2025-08-03 12:55:09 -0600

    refactor: Remove Stripe API route to fix build and revert to Buy Button

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -7,17 +7,20 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
 console.log("Hello from Functions!")
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
+addEventListener("fetch", async (event) => {
+  const req = event.request;
+  const { name } = await req.json();
   const data = {
     message: `Hello ${name}!`,
-  }
+  };
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  event.respondWith(
+    new Response(
+      JSON.stringify(data),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  );
+});
 
 /* To invoke locally:
 
